d37f96b1427daf93f28ae2a4536e06588e0dc001
zwaxi <muhammadishkuvatov@gmail.com>
2024-04-18 14:01:15 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let name = prompt("ismingizni kiriting. . .")
+let surname = prompt("familiyangizni kiriting . . .")
+let age = prompt("yoshingizni kiriting . . .")
+let home = prompt ("qatta yashaysiz . . .")
+let subjekt = prompt ("yaxshi korgan narsangiz . . .")
+
+
+console.log(name);
+console.log(surname);
+console.log(age);
+console.log(home);
+console.log(subjekt);
